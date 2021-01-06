0ec66cec713b529b05d8f6f05a041d09ce02309a
AshMiass <ashmiass@gmail.com>
2021-01-06 19:03:32 +0000

    add resposive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      'header-pattern': "url('assets/epil_img1.jpg')"
+    },
   },
   variants: {
     extend: {},
